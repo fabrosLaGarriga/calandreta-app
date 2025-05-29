37e825e4b364fc42194781308c1c3187442da0e4
fabrosLaGarriga <fabrossard@gmail.com>
2025-05-29 22:01:51 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+function App() {
+  return (
+    <div>
+      <h1>Bienvenue sur l'app Calandreta</h1>
+      <p>Ceci est un test de déploiement avec Vercel.</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
