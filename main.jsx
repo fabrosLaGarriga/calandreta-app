fbd4e96ebfbd7df0f34366ddcf9456581ff4b775
fabrosLaGarriga <fabrossard@gmail.com>
2025-06-01 20:47:17 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Pages simples pour le moment
+function Admin() {
+  return <h1>Espace Admin – Publication des infos</h1>
+}
+
+function Familles() {
+  return <h1>Espace Familles – Actualités de l’école</h1>
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/familles" element={<Familles />} />
+        {/* On peut ajouter d'autres routes plus tard */}
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+À l'instant
+
