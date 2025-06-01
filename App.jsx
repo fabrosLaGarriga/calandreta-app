7bf4f07156ffe244c4ea21972c98673ae2d33182
fabrosLaGarriga <fabrossard@gmail.com>
2025-06-01 20:37:47 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,15 @@
+// src/App.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Admin from "./pages/Admin";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
