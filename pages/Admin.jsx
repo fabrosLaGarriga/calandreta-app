b08b75420f9b200d6dcad22119080533575420fb
fabrosLaGarriga <fabrossard@gmail.com>
2025-06-01 20:38:49 +0200

    Update Admin.jsx

--- pages/Admin.jsx
+++ pages/Admin.jsx
@@ -1,1 +1,9 @@
-
+// src/pages/Admin.jsx
+export default function Admin() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Espace Admin</h1>
+      <p>Ajoutez ici les actualités pour les familles.</p>
+    </div>
+  );
+}
